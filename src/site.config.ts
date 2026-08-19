d46cf968ebae4642344aeabcfc56a6b7a026f1a9
phantomfancy <89564432+phantomfancy@users.noreply.github.com>
2026-08-19 18:45:44 +0800

    setup: 更新favicon,avatar和site config

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,21 +3,21 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // [Basic]
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: 'Astro Theme Pure',
+  title: 'lichloride: phant\'s blog',
   /** Will be used in index page & copyright declaration */
-  author: 'Pure Lab',
+  author: 'phantomfancy',
   /** Description metadata for your website. Can be used in page metadata. */
-  description: 'Stay hungry, stay foolish',
+  description: 'You live here, I work here...',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
   favicon: '/favicon/favicon.ico',
   /** The default social card image for your site which should be a path to an image in the `public/` directory. */
   socialCard: '/images/social-card.png',
   /** Specify the default language for this site. */
   locale: {
-    lang: 'en-US',
-    attrs: 'en_US',
+    lang: 'zh-CN',
+    attrs: 'zh-CN',
     // Date locale
-    dateLocale: 'en-US',
+    dateLocale: 'zh-CN',
     dateOptions: {
       day: 'numeric',
       month: 'short',
